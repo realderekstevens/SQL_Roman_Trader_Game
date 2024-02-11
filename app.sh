ea163f73ee89b0239a28cbe16121a07f053463de
Derek <72669839+realderekstevens@users.noreply.github.com>
2024-02-10 19:30:58 -0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,44 +1,48 @@
 #!/bin/bash
-PSQL="psql -X --username=postgres --dbname=blackjack --tuples-only -c"
-echo -e "\n~~~~~ Blackjack Simulator ~~~~~\n"
-
+PSQL="psql -X --username=postgres --dbname=tradergame --tuples-only -c"
+echo -e "\n~~~~~ Roman Empire Trader Game ~~~~~\n"
 MAIN_MENU() {
 	if [[ $1 ]]
 	then
 		echo -e "\n$1"
 	fi
-
 	echo "What would you like to do?"
-	echo -e "\n1. New Hand\n2. Discard All Cards\n3. Add New Deck\n4. Display Card Count\n5. Format PostgreSQL\n6. Display Cards in Deck\n0. Exit"
+	echo -e "\n1. Create one person\n2. Delete all the People\n3. Create Tables\n4. Drop All Tables\n0. Exit"
 	read MAIN_MENU_SELECTION
 
 	case $MAIN_MENU_SELECTION in
-		1) NEW_HAND ;;
-		2) DISCARD_ALL_CARDS ;;
-		3) ADD_NEW_DECK ;;
-		4) DISPLAY_CARD_COUNT ;;
-		5) FORMAT_POSTGRESQL ;;
-		6) DISPLAY_CARDS_IN_DECK ;;
+		1) NEW_PERSON ;;
+		2) TRUNCATE_TABLE_PEOPLE ;;
+		3) CREATE_TABLES ;;
+		4) DROP_TABLES ;;
 		0) EXIT ;;
 		*) MAIN_MENU "Please enter a valid option." ;;
 	esac
 }
 
-NEW_HAND(){
-echo -e "\nThank you for stopping in.\n"
+NEW_PERSON(){
+for i in {1..3000}
+do
+	echo "$i / 3000"
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 1)";
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 2)";
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 3)";
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 4)";
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 5)";
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 6)";
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 7)";
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 8)";
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 9)";
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 10)";
+	$PSQL "INSERT INTO people(first_name, last_name, location_id) VALUES('Julias', 'Caeser', 11)";
+	clear
+done
 MAIN_MENU
 }
 
-DISCARD_ALL_CARDS(){
-echo -e "\nDiscarding all cards.\n"
-$PSQL "TRUNCATE TABLE cards;"
-MAIN_MENU
-}
-
-
-ADD_NEW_DECK(){
-$PSQL "INSERT INTO cards(face_id, suit_id, location_id) VALUES(1, 1, 1), (2, 1, 1), (3, 1, 1), (4, 1, 1), (5, 1, 1), (6, 1, 1), (7, 1, 1), (8, 1, 1), (9, 1, 1), (10, 1, 1), (11, 1, 1), (12, 1, 1), (13, 1, 1), (1, 2, 1), (2, 2, 1), (3, 2, 1), (4, 2, 1), (5, 2, 1), (6, 2, 1), (7, 2, 1), (8, 2, 1), (9, 2, 1), (10, 2, 1), (11, 2, 1), (12, 2, 1), (13, 2, 1), (1, 3, 1), (2, 3, 1), (3, 3, 1), (4, 3, 1), (5, 3, 1), (6, 3, 1), (7, 3, 1), (8, 3, 1), (9, 3, 1), (10, 3, 1), (11, 3, 1), (12, 3, 1), (13, 3, 1), (1, 4, 1), (2, 4, 1), (3, 4, 1), (4, 4, 1), (5, 4, 1), (6, 4, 1), (7, 4, 1), (8, 4, 1), (9, 4, 1), (10, 4, 1), (11, 4, 1), (12, 4, 1), (13, 4, 1)";
-echo -e "\nAdded new deck.\n"
+TRUNCATE_TABLE_PEOPLE(){
+echo -e "\nDiscarding all the people.\n"
+$PSQL "TRUNCATE TABLE people;"
 MAIN_MENU
 }
 
@@ -50,19 +54,93 @@ MAIN_MENU
 DISPLAY_CARD_COUNT(){
 $PSQL "SELECT SUM(card_count) FROM cards;"
 $PSQL "SELECT SUM(card_count_complex) FROM cards;"
-
-echo -e "\nThank you for stopping in.\n"
 MAIN_MENU
 }
 
-FORMAT_POSTGRESQL(){
-$PSQL "DROP TABLE IF EXISTS faces CASCADE; DROP TABLE IF EXISTS locations CASCADE; DROP TABLE IF EXISTS suits CASCADE; DROP TABLE IF EXISTS cards CASCADE; DROP TABLE IF EXISTS scores CASCADE;"
-$PSQL "CREATE TABLE locations(location_id SMALLINT GENERATED ALWAYS AS IDENTITY, location_name VARCHAR(255) NOT NULL, PRIMARY KEY(location_id));"
-$PSQL "CREATE TABLE suits(suit_id SMALLINT GENERATED ALWAYS AS IDENTITY, suit_name VARCHAR(255) NOT NULL, PRIMARY KEY(suit_id));"
-$PSQL "CREATE TABLE faces(face_id SMALLINT GENERATED ALWAYS AS IDENTITY, face_name VARCHAR(255) NOT NULL, face_value SMALLINT NOT NULL, PRIMARY KEY(face_id)); 
-$PSQL "INSERT INTO faces(face_name, face_value) VALUES('Ace', 1), ('Two', 2), ('Three', 3), ('Four', 4), ('Five', 5), ('Six', 6), ('Seven', 7), ('Eight', 8), ('Nine', 9), ('Ten', 10), ('Jack', 10), ('Queen', 10), ('King', 10);"
-$PSQL "CREATE TABLE cards(card_id INT GENERATED ALWAYS AS IDENTITY, face_id SMALLINT, suit_id SMALLINT, location_id SMALLINT, PRIMARY KEY(card_id), CONSTRAINT fk_face FOREIGN KEY(face_id) REFERENCES faces(face_id) ON DELETE CASCADE, CONSTRAINT fk_suit FOREIGN KEY(suit_id) REFERENCES suits(suit_id) ON DELETE CASCADE, CONSTRAINT fk_location FOREIGN KEY(location_id) REFERENCES locations(location_id) ON DELETE CASCADE);"
-$PSQL "CREATE TABLE scores(score_id INT GENERATED ALWAYS AS IDENTITY, turn_id INT, location_id SMALLINT, score int NOT NULL, PRIMARY KEY(score_id));"
+CREATE_TABLES(){
+
+$PSQL "CREATE TABLE locations(
+	location_id INT GENERATED ALWAYS AS IDENTITY,
+   	location_name varchar(255),
+   	location_x int,
+   	location_y int,
+   	population int);"
+
+$PSQL "INSERT INTO locations(
+location_name, location_x, location_y) 
+VALUES('Rome', 12, 42), 
+('Alexandria', 30, 31),
+('London', 0, 52),
+('Constantinople', 29, 41),
+('Antioch', 36, 36),
+('Ravenna', 12, 44),
+('Mediolanum', 9, 45),
+('Ephesus', 27, 38),
+('Carthage', 91, 40),
+('Lugdunum', 5, 46),
+('Thessaloniki', 23, 41);"
+
+$PSQL "CREATE TABLE people(
+	person_id INT GENERATED ALWAYS AS IDENTITY,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	gender_male BOOLEAN DEFAULT 'y',
+	employed BOOLEAN DEFAULT 'n',
+	employer_id int,
+	location_id int,
+	title VARCHAR(255) DEFAULT 'Citizen',
+	hunger int DEFAULT 50,
+	satisfaction int DEFAULT 50,
+	politics_x int DEFAULT 50,
+	politics_y int DEFAULT 50);"
+
+$PSQL "CREATE TABLE roman_names(
+	name_id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(255));"
+
+$PSQL "INSERT INTO roman_names(
+name) 
+VALUES('Abronius'),
+('Abudius'),
+('Aburius'),
+('Accius'),
+('Accoleius'),
+('Acerronius'),
+('Acilius'),
+('Aconius'),
+('Actorius'),
+('Acutius'),
+('Adginnius'),
+('Aebutius'),
+('Aedinius'),
+('Aelius'),
+('Aemilius'),
+('Aerelius'),
+('Afinius'),
+('Afranius'),
+('Agnanius'),
+('Agorius'),
+('Albanius'),
+('Albatius'),
+('Albinius'),
+('Albius'),
+('Albinovanus'),
+('Albucius'),
+('Alburius'),
+('Alfenus'),
+('Alfius'),
+('Allectius'),
+('Allienus'),
+('Amafinius'),
+('Amatius'),
+('Amblasius');"
+MAIN_MENU
+}
+
+DROP_TABLES(){
+$PSQL "DROP TABLE people;"
+$PSQL "DROP TABLE locations;"
+$PSQL "DROP TABLE roman_names;"
 MAIN_MENU
 }
 
